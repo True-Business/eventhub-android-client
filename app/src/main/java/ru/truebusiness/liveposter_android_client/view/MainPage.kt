5e2e3ed9d02dd1cd4f7abeea984f7164323c5b45
e.vartazaryan <e.vartazaryan@intabia.ru>
2024-11-04 23:08:19 +0700

    added main body with scrolling

--- app/src/main/java/ru/truebusiness/liveposter_android_client/view/MainPage.kt
+++ app/src/main/java/ru/truebusiness/liveposter_android_client/view/MainPage.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -13,9 +16,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 // TODO(e.vartazaryan): надо будет переделать через получение категорий с бекенда
+// TODO(e.vartazaryan): надо заменить список из "Привет" на загрузку мероприятий с бекенда
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -48,6 +53,18 @@ fun MainPage() {
                 .fillMaxSize()
                 .background(Color.White),
             contentAlignment = Alignment.Center
-        ) {}
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(100) { index ->
+                    Text(
+                        text = "Привет${index + 1}",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
     }
 }
