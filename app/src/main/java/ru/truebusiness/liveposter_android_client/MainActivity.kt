92ba726f18ff4fbde7d032b9fbcb39f7255c02b2
e.vartazaryan <e.vartazaryan@intabia.ru>
2024-11-04 18:55:11 +0700

    added main page

--- app/src/main/java/ru/truebusiness/liveposter_android_client/MainActivity.kt
+++ app/src/main/java/ru/truebusiness/liveposter_android_client/MainActivity.kt
@@ -4,44 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.truebusiness.liveposter_android_client.ui.theme.LiveposterandroidclientTheme
+import ru.truebusiness.liveposter_android_client.view.MainPage
 
-class MainActivity : ComponentActivity() {
+class MainActivity: ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LiveposterandroidclientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainPage()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LiveposterandroidclientTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/ru/truebusiness/liveposter_android_client/view/MainPage.kt
@@ -0,0 +1,51 @@
+package ru.truebusiness.liveposter_android_client.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainPage() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Категории",
+                    fontSize = 15.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            actions = {
+                Button(onClick = {}) {
+                    Text("Кино")
+                }
+                Button(onClick = {}) {
+                    Text("Музыка")
+                }
+                Button(onClick = {}) {
+                    Text("Рестораны")
+                }
+            }
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {}
+    }
+}
