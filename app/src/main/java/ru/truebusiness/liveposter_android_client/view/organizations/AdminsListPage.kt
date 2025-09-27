0eaefb583100872804937fb81e9d30507d6c1e67
Ilya <i.anzhin@tbank.ru>
2025-09-27 19:42:53 +0700

    rm unused imports

--- app/src/main/java/ru/truebusiness/liveposter_android_client/view/organizations/AdminsListPage.kt
+++ app/src/main/java/ru/truebusiness/liveposter_android_client/view/organizations/AdminsListPage.kt
@@ -1,7 +1,6 @@
 package ru.truebusiness.liveposter_android_client.view.organizations
 
 import android.annotation.SuppressLint
-import android.view.animation.Transformation
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
@@ -25,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
