d9d70b1f59bdc3ac22a183a126c7ca52aea11253
Eduard Vartazaryan <e.vartazaryan@trueengineering.ru>
2025-10-27 23:50:40 +0700

    fix: fixed project build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+    implementation("androidx.compose.runtime:runtime-livedata:1.9.4")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
@@ -54,9 +53,8 @@ dependencies {
     implementation("net.engawapg.lib:zoomable:2.0.0")
     implementation("com.google.accompanist:accompanist-navigation-animation:0.30.1")
 
-
+    // вместо того, что написано выше, лучше использовать каталоги (то что снизу)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
