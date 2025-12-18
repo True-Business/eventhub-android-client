6d7630dadf0f99a21d8d4ec290422dbdfbd6362f
Ilya Anzhin <109177759+kestfor@users.noreply.github.com>
2025-12-18 14:18:41 +0700

    using real user data/anonymous (#20)

---
+++ app/src/main/java/ru/truebusiness/liveposter_android_client/view/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,30 @@
+package ru.truebusiness.liveposter_android_client.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.truebusiness.liveposter_android_client.repository.AuthRepository
+
+class ProfileViewModelFactory(
+    private val authRepository: AuthRepository
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProfileViewModel::class.java)) {
+            return ProfileViewModel(authRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
+
+class ProfileSettingsViewModelFactory(
+    private val authRepository: AuthRepository
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ProfileSettingsViewModel::class.java)) {
+            return ProfileSettingsViewModel(authRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
+}
+
